85b66f5982faa01ccfa2242131151d94aca7f188
= <=>
2025-04-25 14:56:01 +0700

    fix: "fix config for dynamically set env"

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-const mode: "PROD" | "DEV" = "DEV";
+const mode = process.env.NODE_ENV ?? "DEV";
 const port = process.env.PORT;
 const databaseUrl =
   mode === "DEV" ? process.env.DEV_DATABASE_URL : process.env.PROD_DATABASE_URL;
